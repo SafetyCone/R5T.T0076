6f0ba302a4daada6a64c74977d02538406c8a1ff
davidcoats <david@getsafetycone.com>
2021-10-29 11:16:11 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0076.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0076.Construction
+{
+    /// <summary>
+    /// Construction project for Custom IWebHostBuilder interface and WebHostBuilder class implementation..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0076/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0076
+{
+    /// <summary>
+    /// Custom IWebHostBuilder interface and WebHostBuilder class implementation.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
